c3f791b71f072cf666b4255da90b340ace425a62
Ozan MUDUL <mudulozan@gmail.com>
2024-03-04 22:51:41 +0300

    Lectures 52-63

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.module';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  Ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 }
